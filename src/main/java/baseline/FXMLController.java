98663b6c5d16a6f8e68ba65936b91e31eeea6257
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-10-22 18:10:02 -0400

    First commit. GUI Mockup for Application 1.

---
+++ src/main/java/baseline/FXMLController.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+
+public class FXMLController implements Initializable {
+
+    @FXML
+    private Button button;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        /*
+            Empty, just GUI Mockup for now.
+         */
+    }
+}
+
